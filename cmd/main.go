74b0fdd3c9cc7e2c538ec2d38b9916d1ce05b516
Fernando Castellanos <fernan2cas96@gmail.com>
2023-04-29 22:23:13 -0400

    main: add gin

--- cmd/main.go
+++ cmd/main.go
@@ -27,5 +27,5 @@ func main() {
 		})
 	})
 	r.GET("/ping", dockerController.CreateWordpressService)
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run("0.0.0.0:8088") // listen and serve on 0.0.0.0:8080
 }
