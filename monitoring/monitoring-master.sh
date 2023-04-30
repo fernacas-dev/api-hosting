d6e83057bdab6471aebf5c3cb20b0a318778d2a7
Fernando Castellanos <fernan2cas96@gmail.com>
2023-04-30 14:09:13 -0400

    main: add gin

--- monitoring/monitoring-master.sh
+++ monitoring/monitoring-master.sh
@@ -1,4 +1,3 @@
-
 # !/bin/bash
 
 #docker stop postgres-exporter
@@ -6,7 +5,7 @@
 
 #docker stop keycloak-postgres-exporter
 #docker rm stop keycloak-postgres-exporter
- 
+
 #docker run --name=postgres-exporter \
 # --restart=always -d \
 #  -p 9187:9187 \
@@ -29,20 +28,20 @@ echo "Remove grafana"
 docker stop grafana && docker rm grafana
 
 MY_PATH=/home/cloud/MONITORINGDATA
-ACTUAL=/home/cloud/devops/monitoring
+ACTUAL=/home/cloud/golang_projects/api-hosting/monitoring
 
 echo "Start prometheus"
 
 docker run -p 9090:9090 --name=prometheus --restart=always \
- -v $ACTUAL/prometheus/config.yml:/etc/prometheus/prometheus.yml \
- -v $MY_PATH/prometheus_db/var/lib:/var/lib/prometheus \
- -v $MY_PATH/prometheus_db/prometheus:/prometheus \
- -v $MY_PATH/prometheus_db/alert.rules:/etc/prometheus/alert.rules \
- -d prom/prometheus \
- --config.file=/etc/prometheus/prometheus.yml \
- --web.route-prefix=/ \
- --storage.tsdb.retention.time=200h \
- --web.enable-lifecycle
+  -v $ACTUAL/prometheus/config.yml:/etc/prometheus/prometheus.yml \
+  -v $MY_PATH/prometheus_db/var/lib:/var/lib/prometheus \
+  -v $MY_PATH/prometheus_db/prometheus:/prometheus \
+  -v $MY_PATH/prometheus_db/alert.rules:/etc/prometheus/alert.rules \
+  -d prom/prometheus \
+  --config.file=/etc/prometheus/prometheus.yml \
+  --web.route-prefix=/ \
+  --storage.tsdb.retention.time=200h \
+  --web.enable-lifecycle
 
 #echo "Start node-exporter"
 #docker run -p 9100:9100 --name=node-exporter --restart=always \
@@ -61,7 +60,6 @@ docker run -p 9090:9090 --name=prometheus --restart=always \
 
 echo "Start grafana"
 
-docker run -p 3000:3000 -e GF_SECURITY_ADMIN_PASSWORD=DontTouchMyGrafana2021* --name=grafana  --restart=always \
- -v $MY_PATH/grafana_db:/var/lib/grafana \
- -d grafana/grafana
-
+docker run -p 3000:3000 -e GF_SECURITY_ADMIN_PASSWORD=DontTouchMyGrafana2021* --name=grafana --restart=always \
+  -v $MY_PATH/grafana_db:/var/lib/grafana \
+  -d grafana/grafana
